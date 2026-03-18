918479d3d5d86417afbbcb41f95e53e4cc00abfe
Krushna-B <Krushna.Bhanushali@unc.edu>
2026-03-18 15:07:57 -0400

    Switch to sourcing R/ files directly instead of installing RNAPeaks package

--- deployment/global.R
+++ deployment/global.R
@@ -6,8 +6,7 @@ options(shiny.maxRequestSize = 500 * 1024^2)
 # Set BioConductor repositories
 options(repos = BiocManager::repositories())
 
-# Load RNAPeaks and its dependencies
-library(RNAPeaks)
+# Core packages
 library(rtracklayer)
 library(ggplot2)
 library(dplyr)
@@ -21,6 +20,33 @@ library(scales)
 library(grid)
 library(slider)
 library(magrittr)
+
+# Source RNAPeaks functions directly from the package R/ directory
+r_dir <- file.path(dirname(getwd()), "R")
+
+source(file.path(r_dir, "BD_CheckBed.R"))
+source(file.path(r_dir, "BD_FilterBed.R"))
+source(file.path(r_dir, "BD_OrderPeak.R"))
+source(file.path(r_dir, "BD_Prepare_Bed.R"))
+source(file.path(r_dir, "BD_AnotBed.R"))
+
+source(file.path(r_dir, "GN_GetGene.R"))
+source(file.path(r_dir, "GN_Build_Gene_Structure_Helper_Functions.R"))
+source(file.path(r_dir, "GN_Compute_Intron_Positions.R"))
+source(file.path(r_dir, "GN_Make_Intron_Arrows.R"))
+source(file.path(r_dir, "GN_Get_Plot_by_Gene.R"))
+source(file.path(r_dir, "GN_draw_Gene_Plot.R"))
+source(file.path(r_dir, "GN_PlotGene.R"))
+
+source(file.path(r_dir, "RG_GetRegion.R"))
+source(file.path(r_dir, "RG_Build_Region_Structure.R"))
+source(file.path(r_dir, "RG_Make_Intron_Arrows.R"))
+source(file.path(r_dir, "RG_Plot_Region_MultiGene.R"))
+source(file.path(r_dir, "RG_PlotRegion.R"))
+
+source(file.path(r_dir, "SM_HelperFunctions.R"))
+source(file.path(r_dir, "SM_CreateSequenceMap.R"))
+source(file.path(r_dir, "Splicing_Map.R"))
 
 
 
